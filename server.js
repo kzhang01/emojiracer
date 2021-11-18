25177675bfbcbb0febdda39ef0cec8881fe3e5a9
Garrett Lu <gbl254@nyu.edu>
2021-11-18 09:49:09 -0500

    setup

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+app.use(express.static('public'));
+// server code goes here!
+// first listen for connection using io.on
+// then... within callback, use socket.on, socket.emit, socket.broadcast, etc.
+// NOTE THAT WE ARE LISTENING WITH server, NOT app!
+
+// store position of both emoji on server, send on each connection
+// client sends back each movement
+
+server.listen(3000);
