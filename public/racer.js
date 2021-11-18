16a04997767bfeb566eedd765fc15a57e8ba8bfd
Garrett Lu <gbl254@nyu.edu>
2021-11-18 10:12:26 -0500

    fixed errors

--- public/racer.js
+++ public/racer.js
@@ -2,6 +2,14 @@
 const socket = io();
 
 function handleClick() {
+    console.log("hello");
     const buttonClicked = this.className;
-    io.emit("click", { buttonClicked: buttonClicked });
+    socket.emit("click", { buttonClicked: buttonClicked });
 }
+
+function main() {
+    document.querySelector(".player1Btn").addEventListener("click", handleClick);
+    document.querySelector(".player2Btn").addEventListener("click", handleClick);
+}
+
+document.addEventListener("DOMContentLoaded", main);
